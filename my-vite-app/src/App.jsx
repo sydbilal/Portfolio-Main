eda2c9bd8e3c6e49e6b7cee6516e79cfdbc53a46
sydbilal <bilalnadeem09876@gmail.com>
2024-09-03 15:30:31 +0500

    homepage animations

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Components/Navbar';
 import Projects from './Components/Projects';
 import Experience from './Components/Experience';
 import ContactMe from './Components/Contactme';
 import './App.css'
 import Home from './Components/Home';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the AOS styles
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 500, // Animation duration in milliseconds
+      once: false,     // Whether animation should happen only once - while scrolling down
+    });
+  }, []);
+  
   return (
     <div>
       <Navbar/>
 
       <section id="home" className="py-5 bg-gray-100">
-      <Home/>
+  <Home />
+</section>
 
-      </section>
-      
+<section id="projects" className="py-5 bg-gray-100" data-aos="fade-down">
+  <Projects />
+</section>
 
+<section id="experience" className="py-1 bg-gray-100" data-aos="fade-down">
+  <Experience />
+</section>
 
-
-      <section id="projects" className="py-5 bg-gray-100">
-        <Projects/>
-      </section>
-      <section id="experience" className="py-1 bg-gray-100">
-        <Experience/>
-      </section>
-      <section id="contact" className="py-5 bg-gray-100">
-        <ContactMe/>
-      </section>
+<section id="contact" className="py-5 bg-gray-100" data-aos="fade-down">
+  <ContactMe />
+</section>
     </div>
   );
 };
